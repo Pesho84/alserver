6be9b9c74923ee1cb9a2796264d722caf6d8c591
al3css <ionutalexandru1992@yahoo.ro>
2017-05-09 19:20:32 +0300

    ArchSvrPack 1

---
+++ SQL/RPGWT.sql
@@ -0,0 +1,27 @@
+insert into rpgwt (SERVERID, CHARTID, WDATE, CURCONN, MAXCONN)
+values ('Zian', 96, null, null, null);
+
+insert into rpgwt (SERVERID, CHARTID, WDATE, CURCONN, MAXCONN)
+values ('Zian', 8, null, null, null);
+
+insert into rpgwt (SERVERID, CHARTID, WDATE, CURCONN, MAXCONN)
+values ('Zian', 3, null, null, null);
+
+insert into rpgwt (SERVERID, CHARTID, WDATE, CURCONN, MAXCONN)
+values ('Zian', 4, null, null, null);
+
+insert into rpgwt (SERVERID, CHARTID, WDATE, CURCONN, MAXCONN)
+values ('Zian', 6, null, null, null);
+
+insert into rpgwt (SERVERID, CHARTID, WDATE, CURCONN, MAXCONN)
+values ('Zian', 1, null, null, null);
+
+insert into rpgwt (SERVERID, CHARTID, WDATE, CURCONN, MAXCONN)
+values ('Zian', 9, null, null, null);
+
+insert into rpgwt (SERVERID, CHARTID, WDATE, CURCONN, MAXCONN)
+values ('Zian', 460, null, null, null);
+
+insert into rpgwt (SERVERID, CHARTID, WDATE, CURCONN, MAXCONN)
+values ('Zian', 0, null, null, null);
+
